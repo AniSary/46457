ef72fdce318eaaa9a88f7ad49e2515061cf0c977
AniSary <dymowvadim@gmail.com>
2025-05-22 13:08:31 +0200

    feat: implement auth screens (register, login, profile) and routes №2

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './navigation/AppNavigator';
+import { PlacesProvider } from './context/PlacesContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <PlacesProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </PlacesProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
